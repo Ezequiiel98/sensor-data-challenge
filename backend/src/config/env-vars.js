a3f7d3f3ce8184a83d068e69868d853e085a2ba0
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-03-29 19:20:16 -0300

    envieroment vars to database added

--- backend/src/config/env-vars.js
+++ backend/src/config/env-vars.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
   PORT_APP: process.env.APP_PORT || '3000',
   NODE_ENV: process.env.NODE_ENV,
   ENV_IS_DEV: process.env.NODE_ENV === 'development',
+  DB: {
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'root',
+    DB_NAME: process.env.DB_NAME || 'default',
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_POST: process.env.DB_PORT || '3306',
+  },
 };
