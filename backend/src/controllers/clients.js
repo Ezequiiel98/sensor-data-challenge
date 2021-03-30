9901106ab214bfabf89e78caa409046cb0f057da
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-03-30 20:27:44 -0300

    controller to get all clients, finished

--- backend/src/controllers/clients.js
+++ backend/src/controllers/clients.js
@@ -1,11 +1,15 @@
-const { Op } = require('sequelize');
 const { ENV_VARS } = require('../config');
 const { CustomError } = require('../utils');
 
 const db = require('../models');
 const Client = require('../models/client')(db.sequelize, db.Sequelize);
 
-const getAllClients = async (req) => ({ clients: [] });
+const getAllClients = async (req) => {
+  const { userId } = req;
+  const clients = await Client.findAll({ where: { userId } });
+
+  return { clients };
+};
 
 module.exports = {
   getAllClients,
