9585d091bd260baa3066eb28241c5852c2ffd9fb
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-03-30 01:25:03 -0300

    logic to find the user and verify his password, added

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,4 +1,41 @@
-const login = (req, res) => 'login';
+const { Op } = require('sequelize');
+
+const { CustomError, verifyPassword } = require('../utils');
+
+const db = require('../models');
+const User = require('../models/user')(db.sequelize, db.Sequelize);
+
+const login = async (req) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: {
+      [Op.or]: [
+        { username },
+        { email: username },
+      ],
+    },
+  });
+
+  const error = new CustomError({
+    message: 'Username or password is wrong',
+    status: 404,
+  });
+
+  if (!user) throw error;
+
+  const passwordsMatch = verifyPassword(password, user.password);
+
+  if (!passwordsMatch) throw error;
+
+  const dataUser = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+  };
+
+  return { user: dataUser };
+};
 
 module.exports = {
   login,
--- backend/src/utils/index.js
+++ backend/src/utils/index.js
@@ -2,4 +2,5 @@ module.exports = {
   Response: require('./Response'),
   CustomError: require('./CustomError'),
   controllerWithTryCatch: require('./controllerWithTryCatch'),
+  verifyPassword: require('./verifyPassword'),
 };
