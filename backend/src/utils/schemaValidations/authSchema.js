18e1670a9f78d63740f7fb0f16a588cc02957686
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-03-30 00:19:59 -0300

    validation to username added

--- backend/src/utils/schemaValidations/authSchema.js
+++ backend/src/utils/schemaValidations/authSchema.js
@@ -1,10 +1,10 @@
 const { check } = require('express-validator');
 
 const login = [
-  check('username').isString().withMessage('Username must be a string'),
+  check('username').isLength({ min: 3 }).withMessage('Username must be a string at least 3 characters'),
   check('password').isLength({ min: 6 }).withMessage('Password must be at least 8 characters'),
 ];
 
 module.exports = {
   login,
-}
+};
