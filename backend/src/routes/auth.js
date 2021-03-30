7a2bfe682ed977d2cdea72873f9fb9cad16e8e38
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-03-30 04:24:47 -0300

    route getMe added

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,7 +3,11 @@ const express = require('express');
 const authRouter = express.Router();
 const { controllerWithTryCatch } = require('../utils');
 
-const { bodyValidator } = require('../middlewares');
+const {
+  bodyValidator,
+  validateToken,
+  decodeToken,
+} = require('../middlewares');
 const { authSchema } = require('../utils/schemaValidations');
 
 const { authController } = require('../controllers');
@@ -13,6 +17,18 @@ authRouter.post(
   bodyValidator(authSchema.login),
   controllerWithTryCatch({
     callback: authController.login,
+    statusSuccess: 200,
+  }),
+);
+
+authRouter.get(
+  '/me',
+  [
+    decodeToken,
+    validateToken,
+  ],
+  controllerWithTryCatch({
+    callback: authController.getMe,
     statusSuccess: 200,
   }),
 );
