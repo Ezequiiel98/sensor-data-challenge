fcec748d108b44d363e578a189a90dec0e8b19d7
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-03-30 20:18:41 -0300

    typo in models user fixed

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Clients, {
+      User.hasMany(models.Client, {
         foreignKey: 'userId',
         as: 'clients',
       });
