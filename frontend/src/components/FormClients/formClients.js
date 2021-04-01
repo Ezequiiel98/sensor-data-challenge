5a8e9163225d898c3f8874be2ff3c697e6fd86e4
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-04-01 04:18:57 -0300

    I added button text depending on whether it is updating create or loading

--- frontend/src/components/FormClients/formClients.js
+++ frontend/src/components/FormClients/formClients.js
@@ -14,6 +14,8 @@ import { INPUTS } from './constants/inputs';
 import { initialStateForm,  initialStateFormErrors } from './constants/initialStates';
 
 
+// borrar esto cuando agregue el servicio de enviar el form
+const isFetching = false;
 export default function FormClients({ dataClient }) {
   const initDataClient = dataClient ? { 
     ...initialStateForm, 
@@ -22,7 +24,7 @@ export default function FormClients({ dataClient }) {
   const isUpdate = Boolean(dataClient);
   const [formData, setFormData] = useState({...initDataClient});
   const [formErrors, setFormErrors] = useState({...initialStateFormErrors});
-  
+
   const handleChange = ({ target: { name, value } }) => {
     setFormData((lastFormData) => ({ ...lastFormData, [name]: value }));
     setFormErrors((lastFormErrors) => ({ ...lastFormErrors, [name]: null }));
@@ -35,6 +37,8 @@ export default function FormClients({ dataClient }) {
     } 
     return  console.log('create');
   }
+  
+  const textButton = isUpdate ? 'Actualizar Cliente' : 'Crear Cliente';
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -65,7 +69,9 @@ export default function FormClients({ dataClient }) {
           Activo
         </Label>
       </FormGroup>
-      <Button className="btn btn-primary w-100">Submit</Button>
+      <Button type="submit" className="btn btn-primary w-100">
+        { isFetching ?  <i class="fas fa-spinner fa-pulse"></i> : textButton }
+      </Button>
     </Form>
   );
 }
