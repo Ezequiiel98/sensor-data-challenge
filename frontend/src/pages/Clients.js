91b557149a3a120eb0448ce68fa5fbabcd47a0a5
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-04-01 04:02:37 -0300

    add modal, table, button to create a new client, and I started to make function to update, create and delete a client

--- frontend/src/pages/Clients.js
+++ frontend/src/pages/Clients.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import Table from '../components/table/table';
+import React, { useState } from 'react';
+
 import getId from '../utils/uniqueId';
+
+import Table from '../components/table/table';
+import Modal from '../components/Modal/modal';
+import FormClients from '../components/FormClients/formClients';
 
 const mockClients = [
   {
@@ -10,13 +14,13 @@ const mockClients = [
     address: 'some address',
     country: 'some country',
     postalCode: 1321,
-    zone: 'sur',
+    zone: 'centro',
     phone: 12321312,
     fax: 'asdasdas',
     web: 'pepito.com',
     email: 'pepito@gmail.com',
     transitInsurance: 'si',
-    transitCargeInsurance: 'si',
+    transitCargeInsurance: 'no',
     active: false,
   },
   {
@@ -77,18 +81,48 @@ const mockClients = [
     zone: 'sur',
     phone: 12321312,
     fax: 'asdasdas',
-    web: 'pepito.com',
+    web: 'pepito.comasd',
     email: 'pepito@gmail.com',
     transitInsurance: 'si',
-    transitCargeInsurance: 'si',
-    active: false,
+    transitCargeInsurance: 'no',
+    active: true,
   },
 ];
 
 const headItems = ['Razón social', 'Nro. de Ruc', 'Direccion', 'Pais','Ciudad', 'Codigo Postal', 'Zona', 'Fax', 'Email', 'Web', 'Seg. Transitos', 'Seg. Carga Suelta', 'Activo', 'Acciones'];
 
 export default function Clients() {
+  const [showModal, setShowModal] = useState(false);
+  const [itemToUpdate, setItemToUpdate] = useState();
+
+  const handleUpdate = (id) => {
+    setItemToUpdate(mockClients.filter(client => client.id === id)[0]);
+    setShowModal(true);
+  };
+
+  const handleDelete = (id) => console.log(id);
+  
+  const handleCreateNewClient = () => {
+    setItemToUpdate();
+    setShowModal(true);
+  }
+
   return (
-    <Table bodyItems={mockClients} headItems={headItems} />
+    <>
+      <button
+        onClick={handleCreateNewClient}
+        className="mb-4 mt-4 pt-2 pb-2 pl-2 pr-2 btn btn-info"
+      >
+        Crear Nuevo Cliente
+      </button>
+      <Table headItems={headItems} bodyItems={mockClients} onDelete={handleDelete} onUpdate={handleUpdate} />
+      <Modal 
+        title={itemToUpdate ? 'Actualizar Cliente' : 'Crear Cliente'} 
+        setShowModal={setShowModal}
+        showModal={showModal}
+      >
+        <FormClients dataClient={itemToUpdate} />
+      </Modal>
+    </>
   ); 
 }
