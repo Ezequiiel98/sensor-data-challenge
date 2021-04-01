fcb14bdb21842638340ecaf499158b2c59c27a5c
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-04-01 17:11:05 -0300

    add option to validate length if is a number

--- frontend/src/utils/validateLength.js
+++ frontend/src/utils/validateLength.js
@@ -2,9 +2,12 @@ const validateLength = ({
   str, 
   min = 1, 
   max = 100,
-  name = 'Campo' 
+  name = 'Campo',
+  isNumber = false,
 }) => { 
   const  result = { isValid: true, error: null };
+
+  str = isNumber ? str.toString() : str;
 
   if (str < min) {
     result.isValid = false;
