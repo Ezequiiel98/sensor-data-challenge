2cfdf4be6bf874cbd94500bfdbfeb2fb7e7022c9
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-04-02 00:15:14 -0300

    logic to get info user and proctect admin routes, added

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from './context/AuthContext';
+
+import { useHttp } from './hooks/useHttp';
+import Loader from './components/loader/loader';
 import Admin from './admin.jsx';
+import UnauthenticatedHome from './pages/UnauthenticatedHome';
 
 export default function App() {
-  return <Admin />
+  const [dataAuth, setDataAuth] = useContext(AuthContext);
+  const { httpGet, isFetching } = useHttp();
+  
+  const getDataUser = async () => {
+    if (dataAuth.token) {
+      try {
+        const { data: { user } } = await httpGet('/auth/me');
+        setDataAuth({ ...user, token: dataAuth.token })
+      } catch {
+        setDataAuth({});
+        localStorage.removeItem('token');
+      }
+    }
+
+    return null;
+  };
+
+  useEffect(getDataUser, []);
+  
+  if (isFetching) {
+    return (
+     <Loader />  
+  )
+  }
+
+  return (dataAuth.token ? <Admin /> : <UnauthenticatedHome />)
 }
